6654a0cc97a4c7309d43f05309053d453952c709
Adam Bławat <blawat.adam@gmail.com>
2015-10-25 22:58:38 +0100

    Modyfikacja kodu.

--- short-opts-example-1.c
+++ short-opts-example-1.c
@@ -1,49 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdint.h>
+
+/* List of available command-line options */
+enum
+{
+    OPT_A = 0x01,  /* Option '-a', bit 0 */
+    OPT_B = 0x02,  /* Option '-b', bit 1 */
+    OPT_C = 0x04   /* Option '-c', bit 2 */
+};
 
 int main(int argc, char **argv)
 {
+    /*
+    ** String containing the legitimate option characters
+    **
+    ** The first colon character (':') enables detection of missing option argument
+    ** Each single character is an short option (ex. 'a' enables '-a' option)
+    ** Each single character followed by a colon is a short option that requires an argument (ex. 'c:')
+    */
     char optstring[] = ":abc:";
     
-    int a_flag = 0;
-    int b_flag = 0;
+    /* Variable containing bit flags for options */
+    uint8_t options = 0x00;
 
-    char *c_value = NULL;
+    /* Pointer to '-c' option argument */
+    char *opt_c_arg = NULL;
     
     int result;
     
+    /* Disable error messages */
     opterr = 0;
-    
     
     do
     {
+        /* Read next command-line option */
         result = getopt(argc, argv, optstring);
         
         switch (result)
         {
-            case 'a':   a_flag = 1;
+            /* Option '-a' was passed */
+            case 'a':   options |= OPT_A;
                         break;
             
-            case 'b':   b_flag = 1;
+            /* Option '-b' was passed */
+            case 'b':   options |= OPT_B;
                         break;
             
-            case 'c':   c_value = optarg;
+            /* Option '-c' was passed */
+            case 'c':   options |= OPT_C;
+                        opt_c_arg = optarg;
                         break;
             
-            case '?':   fprintf(stderr, "Unrecognized option '-%c'.\n", optopt);
+            /* Unrecognized option was passed */
+            case '?':   fprintf(stderr, "Error: Unrecognized option '-%c'\n", optopt);
                         return 1;
             
-            case ':':   fprintf(stderr, "Option '-%c' requires an argument.\n", optopt);
+            /* Option that requires an argument was passed, but an argument is missing */
+            case ':':   fprintf(stderr, "Error: Option '-%c' requires an argument\n", optopt);
                         return 1;
         }
     }
     while (result != -1);
     
-    printf("Option '-a' = %d\n", a_flag);
-    printf("Option '-b' = %d\n", b_flag);
+    /* Checks if bit flag for option '-a' is set */
+    if (options & OPT_A)
+    {
+        puts("Option '-a' is set");
+    }
     
-    printf("Option '-c' value = %s\n", c_value);
+    /* Checks if bit flag for option '-b' is set */
+    if (options & OPT_B)
+    {
+        puts("Option '-b' is set");
+    }
+    
+    /* Checks if bit flag for option '-c' is set */
+    if (options & OPT_C)
+    {
+        printf("Option '-c' is set with argument '%s'\n", opt_c_arg);
+    }
     
     return 0;
 }
