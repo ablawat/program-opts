9601ae5efd313c11a56fcc83bbc08e9319c7937b
Adam Bławat <blawat.adam@gmail.com>
2015-11-11 21:56:14 +0100

    Modyfikacja kodu.

--- long-opts-example-1.c
+++ long-opts-example-1.c
@@ -1,60 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <getopt.h>
+
+/* List of available command-line options */
+enum
+{
+    OPT_A = 0x01,  /* Option '-a' and '--opta', bit 0 */
+    OPT_B = 0x02,  /* Option '-b' and '--optb', bit 1 */
+    OPT_C = 0x04   /* Option '-c' and '--optc', bit 2 */
+};
 
 int main(int argc, char **argv)
 {
+    /*
+    ** String containing the legitimate option characters
+    **
+    ** The first colon character (':') enables detection of missing option argument
+    ** Each single character is an short option (ex. 'a' enables '-a' option)
+    ** Each single character followed by a colon is a short option that requires an argument (ex. 'c:')
+    */
     char optstring[] = ":abc:";
     
-    int a_flag = 0;
-    int b_flag = 0;
-
-    char *c_value = NULL;
+    /* Variable containing bit flags for options */
+    uint8_t options = 0x00;
     
-    struct option long_options[] =
+    /* Pointer to '-c' and '--optc' option argument */
+    char *opt_c_arg = NULL;
+    
+    /* Definitions of long command-line options */
+    struct option longopts[] =
     {
-        {"opta", no_argument,       0, 'a'},
-        {"optb", no_argument,       0, 'b'},
-        {"optc", required_argument, 0, 'c'},
-        {0, 0, 0, 0}
+        {"opta", no_argument,       NULL, 'a'},  /* Option '--opta' joined with '-a' */
+        {"optb", no_argument,       NULL, 'b'},  /* Option '--optb' joined with '-b' */
+        {"optc", required_argument, NULL, 'c'},  /* Option '--optc' joined with '-c' */
+        {0, 0, 0, 0}                             /* Termination entry */
     };
     
     int result;
     
-    
     do
     {
-        result = getopt_long(argc, argv, optstring, long_options, NULL);
+        /* Read next command-line option */
+        result = getopt_long(argc, argv, optstring, longopts, NULL);
         
         switch (result)
         {
-            case 'a':   a_flag = 1;
+            /* Option '-a' or '--opta'  was passed */
+            case 'a':   options |= OPT_A;
                         break;
             
-            case 'b':   b_flag = 1;
+            /* Option '-b' or '--optb'  was passed */
+            case 'b':   options |= OPT_B;
                         break;
             
-            case 'c':   c_value = optarg;
+            /* Option '-c' or '--optc'  was passed */
+            case 'c':   options |= OPT_C;
+                        opt_c_arg = optarg;
                         break;
             
+            /* Unrecognized option was passed */
             case '?':   if (optopt != 0)
                         {
-                            fprintf(stderr, "Unrecognized option '-%c'.\n", optopt);
+                            fprintf(stderr, "Error: Unrecognized option '-%c'\n", optopt);
                         }
                         else
                         {
-                            fprintf(stderr, "Unrecognized option '%s'.\n", argv[optind - 1]);
+                            fprintf(stderr, "Error: Unrecognized option '%s'\n", argv[optind - 1]);
                         }
                         
                         return 1;
             
+            /* Option that requires an argument was passed, but an argument is missing */
             case ':':   if (*(argv[optind - 1] + 1) != '-')
                         {
-                            fprintf(stderr, "Option '-%c' requires an argument.\n", optopt);
+                            fprintf(stderr, "Error: Option '-%c' requires an argument\n", optopt);
                         }
                         else
                         {
-                            fprintf(stderr, "Option '%s' requires an argument.\n", argv[optind - 1]);
+                            fprintf(stderr, "Error: Option '%s' requires an argument\n", argv[optind - 1]);
                         }
                         
                         return 1;
@@ -62,10 +86,23 @@ int main(int argc, char **argv)
     }
     while (result != -1);
     
-    printf("Option '-a' = %d\n", a_flag);
-    printf("Option '-b' = %d\n", b_flag);
+    /* Checks if bit flag for option '-a' and '--opta' is set */
+    if (options & OPT_A)
+    {
+        puts("Option '-a' or '--opta' is set");
+    }
     
-    printf("Option '-c' value = %s\n", c_value);
+    /* Checks if bit flag for option '-b' and '--optb' is set */
+    if (options & OPT_B)
+    {
+        puts("Option '-b' or '--optb' is set");
+    }
+    
+    /* Checks if bit flag for option '-c' and '--optc' is set */
+    if (options & OPT_C)
+    {
+        printf("Option '-c' or '--optc' is set with argument '%s'\n", opt_c_arg);
+    }
     
     return 0;
 }
