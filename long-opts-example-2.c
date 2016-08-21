51d43c2c15c3970efb35428b6e5a6ca9d92a1517
Adam Bławat <blawat.adam@gmail.com>
2016-08-21 14:17:07 +0200

    Add List of Options and Bit Flags
    Added enumeration of all options for long options example 2. Updated
    options status storage to bit fields. Added detailed comments.

--- long-opts-example-2.c
+++ long-opts-example-2.c
@@ -1,70 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <getopt.h>
+
+/* List of available command-line options */
+enum
+{
+    OPT_A = 0x01,  /* Option '-a' or '--opta', bit 0 */
+    OPT_B = 0x02,  /* Option '-b' or '--optb', bit 1 */
+    OPT_C = 0x04,  /* Option '-c' or '--optc', bit 2 */
+    OPT_D = 0x08,  /* Option '-d' or '--optd', bit 3 */
+    OPT_E = 0x10   /* Option '-e' or '--opte', bit 4 */
+};
 
 int main(int argc, char **argv)
 {
+    /*
+    ** String containing the legitimate option characters
+    **
+    ** The first colon character (':') enables detection of missing option argument
+    ** Each single character is an short option (ex. 'a' enables '-a' option)
+    ** Each single character followed by a colon is a short option that requires an argument (ex. 'd:')
+    */
     char optstring[] = ":abcd:e:";
     
-    int a_flag = 0;
-    int b_flag = 0;
-    int c_flag = 0;
-
-    char *d_value = NULL;
-    char *e_value = NULL;
+    /* Variable containing bit flags for options */
+    uint8_t options = 0x00;
     
+    /* Pointers to '-d' and '-e' option arguments */
+    char *opt_d_arg = NULL;
+    char *opt_e_arg = NULL;
+    
+    /*
+    ** Array of long options definitions
+    **
+    ** Each entry defines one long option and links it to corresponding short option
+    */
     struct option long_options[] =
     {
-        {"opta", no_argument,       0, 'a'},
-        {"optb", no_argument,       0, 'b'},
-        {"optc", no_argument,       0, 'c'},
-        {"optd", required_argument, 0, 'd'},
-        {"opte", required_argument, 0, 'e'},
-        {0, 0, 0, 0}
+        {"opta", no_argument,       0, 'a'},  /* Long option name 'opta', short name 'a' */
+        {"optb", no_argument,       0, 'b'},  /* Long option name 'optb', short name 'b' */
+        {"optc", no_argument,       0, 'c'},  /* Long option name 'optc', short name 'c' */
+        {"optd", required_argument, 0, 'd'},  /* Long option name 'optd', short name 'd' */
+        {"opte", required_argument, 0, 'e'},  /* Long option name 'opte', short name 'e' */
+        {0, 0, 0, 0}                          /* Termination entry */
     };
     
     int result, i;
     
-    
     do
     {
+        /* Read next command-line option */
         result = getopt_long(argc, argv, optstring, long_options, NULL);
         
         switch (result)
         {
-            case 'a':   a_flag = 1;
+            /* Option '-a' or '--opta' was passed */
+            case 'a':   options |= OPT_A;
                         break;
             
-            case 'b':   b_flag = 1;
+            /* Option '-b' or '--optb' was passed */
+            case 'b':   options |= OPT_B;
                         break;
             
-            case 'c':   c_flag = 1;
+            /* Option '-c' or '--optc' was passed */
+            case 'c':   options |= OPT_C;
                         break;
             
-            case 'd':   d_value = optarg;
+            /* Option '-d' or '--optd' was passed */
+            case 'd':   options |= OPT_D;
+                        opt_d_arg = optarg;
                         break;
             
-            case 'e':   e_value = optarg;
+            /* Option '-e' or '--opte' was passed */
+            case 'e':   options |= OPT_E;
+                        opt_e_arg = optarg;
                         break;
             
+            /* Unrecognized option was passed */
             case '?':   if (optopt != 0)
                         {
-                            fprintf(stderr, "Unrecognized option '-%c'.\n", optopt);
+                            fprintf(stderr, "Error: Unrecognized option '-%c'\n", optopt);
                         }
                         else
                         {
-                            fprintf(stderr, "Unrecognized option '%s'.\n", argv[optind - 1]);
+                            fprintf(stderr, "Error: Unrecognized option '%s'\n", argv[optind - 1]);
                         }
                         
                         return 1;
             
+            /* Option that requires an argument was passed, but an argument is missing */
             case ':':   if (*(argv[optind - 1] + 1) != '-')
                         {
-                            fprintf(stderr, "Option '-%c' requires an argument.\n", optopt);
+                            fprintf(stderr, "Error: Option '-%c' requires an argument\n", optopt);
                         }
                         else
                         {
-                            fprintf(stderr, "Option '%s' requires an argument.\n", argv[optind - 1]);
+                            fprintf(stderr, "Error: Option '%s' requires an argument\n", argv[optind - 1]);
                         }
                         
                         return 1;
@@ -72,16 +104,40 @@ int main(int argc, char **argv)
     }
     while (result != -1);
     
-    printf("Option '-a' = %d\n", a_flag);
-    printf("Option '-b' = %d\n", b_flag);
-    printf("Option '-c' = %d\n", c_flag);
+    /* Checks if bit flag for option '-a' and '--opta' is set */
+    if (options & OPT_A)
+    {
+        puts("Option '-a' or '--opta' is set");
+    }
     
-    printf("Option '-d' value = %s\n", d_value);
-    printf("Option '-e' value = %s\n", e_value);
+    /* Checks if bit flag for option '-b' and '--optb' is set */
+    if (options & OPT_B)
+    {
+        puts("Option '-b' or '--optb' is set");
+    }
     
+    /* Checks if bit flag for option '-c' and '--optc' is set */
+    if (options & OPT_C)
+    {
+        puts("Option '-c' or '--optc' is set");
+    }
+    
+    /* Checks if bit flag for option '-d' and '--optd' is set */
+    if (options & OPT_D)
+    {
+        printf("Option '-d' or '--optd' is set with argument '%s'\n", opt_d_arg);
+    }
+    
+    /* Checks if bit flag for option '-e' and '--opte' is set */
+    if (options & OPT_E)
+    {
+        printf("Option '-e' or '--opte' is set with argument '%s'\n", opt_e_arg);
+    }
+    
+    /* Print argv-elements that are not an options */
     for (i = optind; i < argc; i++)
     {
-        printf("No option arg = %s\n", argv[i]);
+        printf("No option argument '%s'\n", argv[i]);
     }
     
     return 0;
